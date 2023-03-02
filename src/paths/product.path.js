c167fa56c29c3c31985eeb6d9615911e8e9513ca
Maria Luísa Brandão <malubrandaor@gmail.com>
2023-03-02 18:09:34 -0300

    feat: requisito 04

--- src/paths/product.path.js
+++ src/paths/product.path.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const productController = require('../controllers/products.cntrll');
+const { nameValidation, minimumLength } = require('../middlewares/validation.products');
 
 const routes = express.Router();
 
 routes.get('/', productController.allProducts);
 routes.get('/:id', productController.productsById);
-routes.post('/', productController.addProduct);
+routes.post('/', nameValidation, minimumLength, productController.addProduct);
 module.exports = routes;
